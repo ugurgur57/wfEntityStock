3276026f079b25309c60d5f4ae942a08cb9a2f15
ugurgur57 <ugurgur57@gmail.com>
2016-07-18 12:38:31 +0300

    StockProjesi

---
+++ wfEntityStock/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace wfEntityStock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbCategories = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUnitsInStock = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtCategoryID = new System.Windows.Forms.TextBox();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbCategories
+            // 
+            this.cbCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCategories.FormattingEnabled = true;
+            this.cbCategories.Location = new System.Drawing.Point(88, 59);
+            this.cbCategories.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbCategories.Name = "cbCategories";
+            this.cbCategories.Size = new System.Drawing.Size(208, 24);
+            this.cbCategories.TabIndex = 0;
+            this.cbCategories.SelectedIndexChanged += new System.EventHandler(this.cbCategories_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kategori Seçiniz";
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.AllowUserToAddRows = false;
+            this.dgvProducts.AllowUserToDeleteRows = false;
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(88, 108);
+            this.dgvProducts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.ReadOnly = true;
+            this.dgvProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProducts.Size = new System.Drawing.Size(651, 250);
+            this.dgvProducts.TabIndex = 2;
+            this.dgvProducts.DoubleClick += new System.EventHandler(this.dgvProducts_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 371);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(341, 371);
+            this.txtProductName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(236, 23);
+            this.txtProductName.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(341, 403);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(236, 23);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 403);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Price";
+            // 
+            // txtUnitsInStock
+            // 
+            this.txtUnitsInStock.Location = new System.Drawing.Point(341, 435);
+            this.txtUnitsInStock.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUnitsInStock.Name = "txtUnitsInStock";
+            this.txtUnitsInStock.Size = new System.Drawing.Size(236, 23);
+            this.txtUnitsInStock.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(237, 435);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "UnitsInStock";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(241, 480);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(72, 31);
+            this.btnNew.TabIndex = 9;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(322, 480);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(79, 31);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Enabled = false;
+            this.btnUpdate.Location = new System.Drawing.Point(410, 480);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(79, 31);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(498, 480);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(79, 31);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtCategoryID
+            // 
+            this.txtCategoryID.Location = new System.Drawing.Point(4, 60);
+            this.txtCategoryID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCategoryID.Name = "txtCategoryID";
+            this.txtCategoryID.ReadOnly = true;
+            this.txtCategoryID.Size = new System.Drawing.Size(36, 23);
+            this.txtCategoryID.TabIndex = 13;
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(4, 371);
+            this.txtProductID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.ReadOnly = true;
+            this.txtProductID.Size = new System.Drawing.Size(36, 23);
+            this.txtProductID.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(833, 522);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.txtCategoryID);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtUnitsInStock);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCategories);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "ADO:Net Entity Dat Model (ORM)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCategories;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUnitsInStock;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtCategoryID;
+        private System.Windows.Forms.TextBox txtProductID;
+    }
+}
+
---
+++ wfEntityStock/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfEntityStock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StockEntities ent = new StockEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllCategories();
+        }
+        private void GetAllCategories()
+        {
+            var Kategoriler = (from k in ent.Categories
+                               select k.name).ToList();
+            cbCategories.DataSource = Kategoriler;
+        }
+        private void cbCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Kategori = cbCategories.SelectedItem.ToString();
+            int CatID = (from k in ent.Categories
+                         where k.name == Kategori
+                         select k.id).First();
+            txtCategoryID.Text = CatID.ToString();
+            GetAllProductsByCategory(CatID);
+        }
+        private void GetAllProductsByCategory(int CategoryID)
+        {
+            var Urunler = (from u in ent.Products
+                           where u.CategoryID == CategoryID
+                           select new { u.Id, u.Name, CategoryName=u.Categories.name, u.Price, u.UnitsInStock, u.CategoryID }).ToList();
+            dgvProducts.DataSource = Urunler;
+            dgvProducts.Columns[1].Width = 150;
+            dgvProducts.Columns["Price"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvProducts.Columns["UnitsInStock"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvProducts.Columns["CategoryID"].Visible = false;
+        }
+        private void dgvProducts_DoubleClick(object sender, EventArgs e)
+        {
+            txtProductID.Text = dgvProducts.SelectedRows[0].Cells[0].Value.ToString();
+            txtProductName.Text = dgvProducts.SelectedRows[0].Cells[1].Value.ToString();
+            txtPrice.Text = dgvProducts.SelectedRows[0].Cells[3].Value.ToString();
+            txtUnitsInStock.Text = dgvProducts.SelectedRows[0].Cells[4].Value.ToString();
+            txtCategoryID.Text = dgvProducts.SelectedRows[0].Cells[5].Value.ToString();
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+            btnSave.Enabled = false;
+            txtProductName.Focus();
+        }
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            btnSave.Enabled = true;
+            Temizle();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtProductName.Text.Trim() != "")
+            {
+                if (ProductControlByName(txtProductName.Text))
+                {
+                    MessageBox.Show("Bu ürün zaten kayıtlı!");
+                    txtProductName.Focus();
+                }
+                else
+                {
+                    Products p = new Products();
+                    p.Name = txtProductName.Text;
+                    p.Price = Convert.ToDecimal(txtPrice.Text);
+                    p.UnitsInStock = Convert.ToInt32(txtUnitsInStock.Text);
+                    p.CategoryID = Convert.ToInt32(txtCategoryID.Text);
+                    ent.Products.Add(p); //Yeni ürün nesnesi arakatmana eklenir.
+                    try
+                    {
+                        ent.SaveChanges(); //Arakatmana göre veritabanı güncellenir.
+                        MessageBox.Show("Ürün bilgileri eklendi.");
+                        btnSave.Enabled = false;
+                        GetAllProductsByCategory(Convert.ToInt32(txtCategoryID.Text));
+                        Temizle();
+                    }
+                    catch (Exception ex)
+                    {
+                        string hata = ex.Message;
+                    }
+                }
+            }
+        }
+        private bool ProductControlByName(string ProductName)
+        {
+            int sayisi = (from p in ent.Products
+                          where p.Name == ProductName
+                          select p).Count();
+            return Convert.ToBoolean(sayisi);
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int degisenID = Convert.ToInt32(txtProductID.Text);
+            var degisen = (from p in ent.Products   //değiştirilecek kayda odaklanır.
+                           where p.Id == degisenID
+                           select p).FirstOrDefault();
+            degisen.Name = txtProductName.Text;
+            degisen.Price = Convert.ToDecimal(txtPrice.Text);
+            degisen.UnitsInStock = Convert.ToInt32(txtUnitsInStock.Text);
+            degisen.CategoryID = Convert.ToInt32(txtCategoryID.Text);
+            try
+            {
+                ent.SaveChanges(); //Arakatmana göre veritabanı güncellenir.
+                MessageBox.Show("Ürün bilgileri güncellendi.");
+                btnUpdate.Enabled = false;
+                btnDelete.Enabled = false;
+                GetAllProductsByCategory(Convert.ToInt32(txtCategoryID.Text));
+                Temizle();
+            }
+            catch (Exception ex)
+            {
+                string hata = ex.Message;
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Silmek İstiyor musunuz?", "SİLİNSİN Mİ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+            {
+                int silinenID = Convert.ToInt32(txtProductID.Text);
+                var silinen = (from p in ent.Products  //Silinecek kayıt gösterilir.
+                               where p.Id == silinenID
+                               select p).FirstOrDefault();
+                ent.Products.Remove(silinen); //Arakatmandan siler.
+                try
+                {
+                    ent.SaveChanges(); //Arakatmana göre veritabanı güncellenir.
+                    MessageBox.Show("Ürün bilgileri silindi.");
+                    btnUpdate.Enabled = false;
+                    btnDelete.Enabled = false;
+                    GetAllProductsByCategory(Convert.ToInt32(txtCategoryID.Text));
+                    Temizle();
+                }
+                catch (Exception ex)
+                {
+                    string hata = ex.Message;
+                }
+            }
+        }
+        private void Temizle()
+        {
+            txtProductName.Clear();
+            txtPrice.Text = "0";
+            txtUnitsInStock.Text = "1";
+            txtProductName.Focus();
+        }
+
+
+
+
+
+
+    }
+}
